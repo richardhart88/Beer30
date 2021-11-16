c5f27f16ec0b8346168be19b85b3800d79b847ee
Richard Hart <tuba1388@yahoo.com>
2021-11-16 17:06:15 -0600

    fixed coordinates

--- app/script.js
+++ app/script.js
@@ -1,7 +1,7 @@
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -38.614, lng: -90.197},
+        center: {lat: 38.614, lng: -90.197},
         zoom: 10,
         mapID: '429960fa87fc711f'
     });
