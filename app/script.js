2e995cbdac6858b6de3f8adb9cf449b618001c67
Richard Hart <tuba1388@yahoo.com>
2021-11-16 16:22:43 -0600

    added map

---
+++ app/script.js
@@ -0,0 +1,12 @@
+let map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -38.614, lng: -90.197},
+        zoom: 10,
+        mapID: '429960fa87fc711f'
+    });
+    }
+
+
+
