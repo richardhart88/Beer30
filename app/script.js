5b464d915338c2a5ffd72314e8107a06d700f266
Richard Hart <tuba1388@yahoo.com>
2022-01-06 11:45:26 -0600

    switched from google maps to mapquest on breweries tab

--- app/script.js
+++ app/script.js
@@ -1,23 +1,32 @@
 
-function initMap() {
+var L;
 
-    //map options
-    var options = {
-        center: {lat: 38.614, lng: -90.197},
-        zoom: 12,
-        mapID: '429960fa87fc711f'
-    }
+window.onload = function() {
+    L.mapquest.key = '0aTGr9Al3b2iHwwRD6018q6Uc6zLP4Ij';
 
-    map = new google.maps.Map(document.getElementById('map'), options);
-        
-    //marker
-    const marker = new google.maps.Marker({
-        position:{lat: 38.6149, lng: -90.1976},
-        map:map
-    
-    });
+    // 'map' refers to a <div> element with the ID map
+var map = L.mapquest.map('map', {
+    center: [38.584487, -90.266699],
+    layers: L.mapquest.tileLayer('hybrid'),
+    zoom: 11
+});
 
-};
+map.addControl(L.mapquest.control({ 
+    position: 'bottomright' 
+}));
+
+L.marker([38.584487, -90.266699], {
+    icon: L.mapquest.icons.marker({
+        primaryColor: '#22407F',
+        secondaryColor: '#3B5998',
+        shadow: true,
+        size: 'sm',
+        symbol: 'A'
+    })
+})
+.bindPopup('This is St. Louis! <a href="https://www.facebook.com"> Here is a link </a>')
+.addTo(map);
+}
 
 // const breweryUrl = 'https://api.openbrewerydb.org/breweries';
 
@@ -37,10 +46,50 @@ function initMap() {
 
 // getBreweries();
 
-fetch("https://api.openbrewerydb.org/breweries")
-    .then(response => response.json())
-    .then(data => {
-            console.log(data)
-            document.querySelector("#breweries").innerText = data
-    })
+// function openForm() {
+//     document.getElementById("myForm").style.display = "block";
+//     }
 
+// function closeForm() {
+//     document.getElementById("myForm").style.display = "none";
+//     }
+
+const openBreweryDb = ("https://api.openbrewerydb.org/breweries")
+
+showBreweries = breweries => {
+    const breweriesDiv = document.querySelector("#breweries");
+        breweries.forEach(data => {
+            const breweryElement = document.createElement('li');
+            breweryElement.innerText = `Brewery Name: ${data.name} + Location:  ${data.city}, ${data.state}`;
+            breweriesDiv.append(breweryElement);
+        })
+}
+
+fetch(openBreweryDb)
+    .then(response => response.json())
+    .then(data => showBreweries(data))
+    .catch((err) => {
+        console.err('Error: ', err);
+    });
+
+// document.querySelector("#newsletter").addEventListener("click", function(noCheck) {
+//     document.getElementById("output-area").innerHTML += "Sorry! You cannot select this at this time!<br>";
+//     noCheck.preventDefault();
+// }, false);
+
+function getParameter( parameterName ) {
+    let parameters = new URLSearchParams( window.location.search );
+    return parameters.get( parameterName );
+};
+
+//button to change background color
+const btn = document.querySelector('.colorBtn');
+
+function random(number) {
+    return Math.floor(Math.random() * (number+1));
+}
+
+btn.addEventListener('click', () => {
+    const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+    document.body.style.backgroundColor = rndCol;
+});
