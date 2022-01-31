b0f75b620cc9f2c6a25f1786e7c556fcd1439a5c
Richard Hart <tuba1388@yahoo.com>
2022-01-31 09:07:22 -0600

    updated sign up page

--- app/script.js
+++ app/script.js
@@ -1,23 +1,36 @@
+// Map info
+var L;
+var markers = [];
 
-// search bar using jQuery
+window.onload = function() {
+    L.mapquest.key = '0aTGr9Al3b2iHwwRD6018q6Uc6zLP4Ij';
 
-// $(document).ready(function() {
-//     $('#searchBar').keyup(function(){
-//         $('#result').html('');
-//         var searchField = $('#searchBar').val();
-//         var expression = new RegExp(searchField, "i");
-//         $.getJSON(url, function(data){
-//             $.each(data, function(key, value){
-//                 if(value.name.search(expression) != -1 || 
-//                     value.city.search(expression) != -1 ||
-//                     value.website_url.search(expression) != -1)
-//                 {
-//                     $('#result').append('<li class="list-group-item"> ' +value.name+ ' <span class="text-muted"> | ' +value.city+ ', ' +value.state+ ' | <a href=' +value.website_url+ '>Visit their website!</a></span></li>');
-//                 }
-//             });
-//         });
-//     });
-// });
+    // 'map' refers to a <div> element with the ID map
+var map = L.mapquest.map('map', {
+    center: [38.584487, -90.266699],
+    layers: L.mapquest.tileLayer('hybrid'),
+    zoom: 11
+});
+
+// moves controls
+map.addControl(L.mapquest.control({ 
+    position: 'bottomright' 
+}));
+
+//default marker
+L.marker([38.584487, -90.266699], {
+    icon: L.mapquest.icons.marker({
+        primaryColor: '#22407F',
+        secondaryColor: '#3B5998',
+        shadow: true,
+        size: 'sm',
+        symbol: 'A'
+    })
+})
+
+.bindPopup('Welcome to St. Louis! <br> Find more info <a href="https://www.stlouis-mo.gov/">  here! </a>')
+.addTo(map);
+}
 
 // search bar using JS
 const breweryList = document.getElementById('result');
@@ -27,16 +40,17 @@ let brewerySpots = [];
 searchBar.addEventListener('keyup', (e) => {
     const searchString = e.target.value.toLowerCase();
 
-
     const filteredBreweries = brewerySpots.filter(data => {
         return (
         data.name.toLowerCase().includes(searchString) || 
-        data.city.toLowerCase().includes(searchString)
+        data.city.toLowerCase().includes(searchString) ||
+        data.state.toLowerCase().includes(searchString)
         );
     });
     displayBreweries(filteredBreweries);
 });
 
+// retrieving data from openbrewerydb.org
 const loadBreweries = async () => {
     try {
         const res = await fetch('https://raw.githubusercontent.com/openbrewerydb/openbrewerydb/master/breweries.json');
@@ -53,7 +67,10 @@ const displayBreweries = (breweries) => {
             return `
             <li class="brewery col">
                 <h3><a href="${brewery.website_url}">${brewery.name}</a></h3>
-                <p>${brewery.city}, ${brewery.state}</p>
+                <address> Location: ${brewery.street}, 
+                ${brewery.city}, ${brewery.state}</address>
+                <p>Type: ${brewery.brewery_type}</p>
+                <a href="#"><i style='font-size:24px' class='fas'>&#xf041;</i></a>
             </li>`
         })
         .join('');
@@ -61,74 +78,6 @@ const displayBreweries = (breweries) => {
 };
 
 loadBreweries();
-
-// Map info
-var L;
-
-window.onload = function() {
-    L.mapquest.key = '0aTGr9Al3b2iHwwRD6018q6Uc6zLP4Ij';
-
-    // 'map' refers to a <div> element with the ID map
-var map = L.mapquest.map('map', {
-    center: [38.584487, -90.266699],
-    layers: L.mapquest.tileLayer('hybrid'),
-    zoom: 11
-});
-
-map.addControl(L.mapquest.control({ 
-    position: 'bottomright' 
-}));
-
-L.marker([38.584487, -90.266699], {
-    icon: L.mapquest.icons.marker({
-        primaryColor: '#22407F',
-        secondaryColor: '#3B5998',
-        shadow: true,
-        size: 'sm',
-        symbol: 'A'
-    })
-})
-
-.bindPopup('Welcome to St. Louis! <br> Find more info <a href="https://www.stlouis-mo.gov/">  here! </a>')
-.addTo(map);
-}
-
-// const breweryUrl = 'https://api.openbrewerydb.org/breweries';
-
-// async function getBreweries() {
-//     const response = await fetch(breweryUrl);
-//     const data = await response.json();
-//     console.log(data);
-
-//     var listOfBreweries ="";
-
-//     data.forEach(function(element) {
-//         listOfBreweries += "<ul>" + "<li>"+ element.name + ', ' + element.city + ', ' + element.state + '.' + "</li>" + "</ul>"
-//     });
-
-//     $('breweries').append(listOfBreweries)
-// };
-
-// getBreweries();
-
-// function openForm() {
-//     document.getElementById("myForm").style.display = "block";
-//     }
-
-// function closeForm() {
-//     document.getElementById("myForm").style.display = "none";
-//     }
-
-// const openBreweryDb = ("https://api.openbrewerydb.org/breweries")
-
-// showBreweries = breweries => {
-//     const breweriesDiv = document.querySelector("#breweries");
-//         breweries.forEach(data => {
-//             const breweryElement = document.createElement('li');
-//             breweryElement.innerText = `Brewery Name: ${data.name} |  Location:  ${data.city}, ${data.state}`;
-//             breweriesDiv.append(breweryElement);
-//         })
-// }
 
 //puts the breweries into a dropdown menu. 
 let dropdown = document.getElementById('breweries-dropdown');
@@ -156,7 +105,7 @@ request.onload = function() {
             dropdown.add(option);
         }
     } else {
-    // Reached the server, but it returned an error
+        alert("This service is currently unavailable.")
     }   
 }
 
@@ -166,27 +115,11 @@ request.onerror = function() {
 
 request.send();
 
-// fetch(openBreweryDb)
-//     .then(response => response.json())
-//     .then(data => showBreweries(data))
-
 function getParameter( parameterName ) {
     let parameters = new URLSearchParams( window.location.search );
     return parameters.get( parameterName );
 };
 
-//button to change background color
-// const btn = document.querySelector('.colorBtn');
-
-// function random(number) {
-//     return Math.floor(Math.random() * (number+1));
-// }
-
-// btn.addEventListener('click', () => {
-//     const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
-//     document.body.style.backgroundColor = rndCol;
-// });
-
 $("#submitForm").click(function() {
     alert("The Form has been Submitted.");
- });
+});
